5f945bf8f929e3b7416cf64573af27f20998b4a9
Daniel Sims <daniel.sims1994@gmail.com>
2021-11-05 13:28:13 +0000

    Added first module + styles

---
+++ app/src/main/java/com/app/navigationprototype/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.app.navigationprototype
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.app.authentication.Greeting
+import com.app.styles.theme.NavigationPrototypeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NavigationPrototypeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting()
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    NavigationPrototypeTheme {
+        Greeting()
+    }
+}
---
+++ authentication/src/main/java/com/app/authentication/LandingScreen.kt
@@ -0,0 +1,9 @@
+package com.app.authentication
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun Greeting() {
+    Text(text = "Hello!")
+}
